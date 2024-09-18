6a8922932e6bd7df4c672a6002627e642b298332
MUKILTEO\1722815 <1722815@mukilteo.wednet.edu>
2024-09-18 13:19:25 -0700

    Added Boarder, Fixed movement, Broken Rotation(needs Fix)

--- Retro Remake/Assets/Test PM.cs
+++ Retro Remake/Assets/Test PM.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class TestPM : MonoBehaviour
+public class Tester : MonoBehaviour
 {
-    public int numberOfItems, Score;        // ints == whole Numbers
-    public float Health, Weight, Speed;     //Floats == Decimal Numbers
-    public string MenuLabel, CharacterName; // Strings == Words/Phrases/text
-    public bool Jump;                       //Bools == values that are True Or False
-    public Vector2 size, PlayerLocation;    //Vector2s == 2-dimensional Decimals
+    public int score, numberOfItems;    // Ints represent WHOLE NUMBERS
+    public float Health, Weight, Speed; // Ints represent DECIMAL NUMBERS
+    public string menuLabel, dialogue, characterName;    // Strings represent WORDS/PHRASES/TEXT
+    public bool isEnemyDead, isEffective, isGrounded, isPlayerDead;    // Btools represent values are TRUE OR FALSE
+    public Vector2 size, playerlocation;  // Vestor2s represent 2-DIMENSIONAL DECIMALS
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Derek's Favorite Student is" + CharacterName);
+        Debug.Log("The person I don't like is" + characterName);
+
+        if (isGrounded) // Will execute the below code IF THE STATEMENT IN THE PARENTHESIS IS CURRENTLY TRUE
+        {
+            Debug.Log("YOW");
+        }
+        else { Debug.Log("Just kidding I actually don't like this person" + ("Hahaha")); }  // Code in the Else will execute if the code in the IF DID NOT EXECUTE
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Retro Remake/Assets/TetrisBlock.cs
+++ Retro Remake/Assets/TetrisBlock.cs
@@ -6,10 +6,12 @@ public class TetrisBlock : MonoBehaviour
 {
     public KeyCode Left;
     public KeyCode Right;
-    public KeyCode Rotate;
+    public  Vector3 RotationPoint;
     public KeyCode Fall;
-    private float previousTime;
+    private float _previousTime;
     public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 30;
     //public KeyCode Rotate;
     // public KeyCode drop;
     // Start is called before the first frame update
@@ -24,18 +26,55 @@ public class TetrisBlock : MonoBehaviour
         if (Input.GetKeyDown(Left))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
         }
 
         else if (Input.GetKeyDown(Right))
         {
             transform.position += new Vector3(1, 0, 0);
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
         }
 
-        if (Time.time - previousTime > (Input.GetKey(Fall) ? fallTime / 10 : fallTime))
+        else if (Input.GetKeyDown(KeyCode.R))
+            {
+            //rotate
+            transform.Rotate(0, 0, 90, Space.Self);
+            if (!ValidMove()) {
+                transform.Rotate(0,0,-90);
+            }
+            }
+        if (Time.time - _previousTime > (Input.GetKey(Fall) ? fallTime / 10 : fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
-            previousTime = Time.time;
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+
+            _previousTime = Time.time;
+
         }
-        
+    }
+
+    bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < -4 || roundedX > 5 || roundedX >= width || roundedY < -10 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
